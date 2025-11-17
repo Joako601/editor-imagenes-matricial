06f53e9e983e526030db1a992e1cb4c0b84670fc
Joako601 <joaquin.uriona@tecdesoftware.edu.mx>
2025-11-17 00:00:04 -0600

    Completar ejercicios de la sección 3, ejercicio 3.3

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -447,7 +447,22 @@ function rotar90Grados(matriz) {
   // Opción 2: Construir directamente la matriz rotada
   //   nuevoPixel[j][alto - 1 - i] = pixelOriginal[i][j]
   
-  return []; // REEMPLAZAR
+const alto = matriz.length;
+  const ancho = matriz[0].length;
+  const nueva = [];
+
+  for (let y = 0; y < ancho; y++) {
+    const fila = [];
+    for (let x = 0; x < alto; x++) {
+      const original = matriz[alto - 1 - x][y];
+      fila.push({
+        r: original.r, g: original.g, b: original.b, a: original.a
+      });
+    }
+    nueva.push(fila);
+  }
+
+  return nueva;
 }
 
 // ============================================
